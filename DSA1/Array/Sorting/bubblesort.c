8a18cd33d20aea236ca2894c49df5b7f1c117e2d
Salman <kans6643@gmail.com>
2026-03-26 04:36:42 +0000

    Pushing sorts

--- DSA1/Array/Sorting/bubblesort.c
+++ DSA1/Array/Sorting/bubblesort.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 
 void Bubblesrt(int arr[], int size){
-    for(int i=1;i<size;i++){
-        int key = arr[i];
-        int j=i-1;
-        while(j>=0 && arr[j]>key){
-            arr[j+1]=arr[j];
-            j--;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
         }
-        arr[j+1]=key;
     }
 }
-
-
 
 
 int main(){
@@ -26,9 +24,10 @@ int main(){
     }
 
     Bubblesrt(arr,size);
-    printf("Sorted array: ");
+    printf("Bubble Sorted array: ");
     for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
+
     return 0;
 }
