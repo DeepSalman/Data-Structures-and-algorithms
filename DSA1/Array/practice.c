6117703d1321c24f4c4f750909842b26b7b2d74b
Salman <kans6643@gmail.com>
2026-03-27 06:28:34 +0000

    practicing number 9 problem

--- DSA1/Array/practice.c
+++ DSA1/Array/practice.c
@@ -1,53 +1,55 @@
 #include<stdio.h>
 #include<math.h>
-#include<string.h>
+#include<limits.h>
 
 
-struct StElm
-{
-    char realValue[20];
-    int sortValue;
-};
-
-void insertionsort(struct StElm A[],int size){
-    for(int i=0;i<size;i++){
-        struct StElm key = A[i];
+void insertionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int key=arr[i];
         int j=i-1;
-        while(j>=0 && A[j].sortValue>key.sortValue){
-            A[j+1]=A[j];
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
             j--;
         }
-        A[j+1]=key;
+        arr[j+1]=key;
     }
 }
 
 int main(){
-    int size;
-    printf("Enter your Char array Size: ");
-    scanf("%d",&size);
-    getchar();
+    int n=5,m=6;
+    int arr1[5]={1,16,13,14,4};
+    int arr2[6]={15,12,5,3,7,22};
 
-    struct StElm A[size];
+    insertionSort(arr1,n);
+    insertionSort(arr2,m);
 
-    printf("Enter your strings: ");
-    for(int i=0;i<size;i++){
-        fgets(A[i].realValue,sizeof(A[i].realValue),stdin);
+    int new1[n+1],new2[m+1];
+
+    for(int i=0;i<n;i++){
+        new1[i]=arr1[i];
+    }
+    for(int i=0;i<m;i++){
+        new2[i]=arr2[i];
     }
 
-    for(int i=0;i<size;i++){
-        A[i].sortValue=strlen(A[i].realValue);
+    new1[n]=INT_MAX;
+    new2[m]=INT_MAX;
+
+    int merge[n+m];
+
+    int i=0,j=0,k=0;
+    for(k=0;k<(n+m);k++){
+        if(new1[i]<new2[j]){
+            merge[k]=new1[i];
+            i++;
+        }
+        else{
+            merge[k]=new2[j];
+            j++;
+        }
     }
-
-    
-    printf("Your provided array: ");
-    for(int i=0;i<size;i++){
-        printf("%s ",A[i].realValue);
-    }
-
-    insertionsort(A,size);
-
-    printf("Sorted array: ");
-    for(int i=0;i<size;i++){
-        printf("%s ",A[i].realValue);
+    printf("Your merged array is :");
+    for(int i=0;i<n+m;i++){
+        printf("%d ",merge[i]);
     }
 }
