5fd8b323f6f9dd1e71a00459863f396b86997694
DeepSalman <rayhanartz@gmail.com>
2026-07-26 09:29:54 +0600

    Maximum Sub array

---
+++ DSA2/DnC/MaximumSubarray.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+#include <climits>
+
+
+int maxCrossSArray(int arr[],int low,int mid, int high){
+    int sum = 0;
+    int leftsum = INT_MIN;
+    for(int i = mid;i>=low;i--){
+        sum+=arr[i];
+        leftsum = max(sum,leftsum);
+    }
+    sum = 0;
+    int rightsum = INT_MIN;
+    for(int i = mid+1;i<=high;i++){
+        sum+=arr[i];
+        rightsum = max(sum,rightsum);
+    }
+
+    return leftsum+rightsum;
+}
+
+int maxSubarray(int arr[], int low, int high){
+    if(low==high) return arr[low];
+    int mid = low+(high-low)/2;
+    int left = maxSubarray(arr,low,mid);
+    int right = maxSubarray(arr,mid+1,high);
+    int cross = maxCrossSArray(arr,low,mid,high);
+
+    return max(left,max(right,cross));
+}
+
+
+int main(){
+    int arr[8] = {2, -4, 3, -1, 5, -6, 1, 2};
+
+    int n = 8;
+
+    cout << maxSubarray(arr, 0, n - 1);
+
+    return 0;
+
+}
---
+++ DSA2/practicefield.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+void mergesort(int arr[],int left,int right){
+
+
+}
+
+
+
+int main(){
+    int arr[10]={10,12,25,-1,11,19,30,-5-2,22};
+    int n=10;
+
+    cout<<"Array Before Sorting"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+
+    cout<<"Array After Sorting"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+
+
+
+}
