9f855f2708c3d9ba64e5ab541d2f61db891e49ed
DeepSalman <rayhanartz@gmail.com>
2026-09-13 04:25:44 +0600

    Prims Algorithm

--- DSA2/Minimum_spanning_tree/prims_algorithm.cpp
+++ DSA2/Minimum_spanning_tree/prims_algorithm.cpp
@@ -1,87 +1,77 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+
 using namespace std;
 
-#define INF 99999
+int main() {
 
-void prim(int cost[][100], int n)
-{
-    bool visited[100] = {false};
-    int minCost[100];
-    int parent[100];
+    int V = 5;
 
-    // Initially, all costs are infinity
-    for (int i = 0; i < n; i++)
-    {
-        minCost[i] = INF;
+    int graph[5][5] = {
+        {0, 2, 3, 0, 0},
+        {2, 0, 1, 4, 0},
+        {3, 1, 0, 5, 6},
+        {0, 4, 5, 0, 7},
+        {0, 0, 6, 7, 0}
+    };
+
+    bool visited[5] = {false};
+
+    int key[5];
+
+    int parent[5];
+
+    for (int i = 0; i < V; i++) {
+        key[i] = INT_MAX;
         parent[i] = -1;
     }
 
-    // Start from vertex 0
-    minCost[0] = 0;
+    key[0] = 0;
 
-    int totalCost = 0;
+    for (int i = 0; i < V; i++) {
 
-    for (int count = 0; count < n; count++)
-    {
-        // Find the unvisited vertex with minimum cost
         int u = -1;
-        int minimum = INF;
 
-        for (int i = 0; i < n; i++)
-        {
-            if (!visited[i] && minCost[i] < minimum)
-            {
-                minimum = minCost[i];
-                u = i;
+        for (int j = 0; j < V; j++) {
+            if (!visited[j] &&
+                (u == -1 || key[j] < key[u])) {
+
+                u = j;
             }
         }
 
-        // Mark vertex as visited
         visited[u] = true;
 
-        // Update costs of adjacent vertices
-        for (int v = 0; v < n; v++)
-        {
-            if (cost[u][v] != 0 &&
+        for (int v = 0; v < V; v++) {
+
+            if (graph[u][v] != 0 &&
                 !visited[v] &&
-                cost[u][v] < minCost[v])
-            {
-                minCost[v] = cost[u][v];
+                graph[u][v] < key[v]) {
+
+                key[v] = graph[u][v];
                 parent[v] = u;
             }
         }
     }
 
-    // Print MST
-    cout << "Edges in MST:\n";
+    int total = 0;
 
-    for (int i = 1; i < n; i++)
-    {
-        cout << parent[i] << " - " << i
-             << " = " << cost[i][parent[i]] << endl;
+    cout << "MST:\n";
 
-        totalCost += cost[i][parent[i]];
+    for (int i = 1; i < V; i++) {
+
+        cout << parent[i]
+             << " - "
+             << i
+             << " : "
+             << key[i]
+             << endl;
+
+        total += key[i];
     }
 
-    cout << "Total Cost = " << totalCost << endl;
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-
-    int cost[100][100];
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> cost[i][j];
-        }
-    }
-
-    prim(cost, n);
+    cout << "Total Cost = " << total << endl;
 
     return 0;
 }
